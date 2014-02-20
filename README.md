d9a263bd5905f64a4c0c0325a4245520875c293c
Michael Wittig <michael.wittig@tullius-walden.com>
2014-02-20 10:05:24 +0100

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+
+[![Build Status](https://secure.travis-ci.org/cinovo/node-isin-validator.png)](http://travis-ci.org/cinovo/node-isin-validator)
+[![NPM version](https://badge.fury.io/js/cinovo-isin-validator.png)](http://badge.fury.io/js/cinovo-isin-validator)
+[![NPM dependencies](https://david-dm.org/cinovo/node-isin-validator.png)](https://david-dm.org/cinovo/node-isin-validator)
+
+# cinovo-isin-validator
+
+## Getting started
+
+	npm install cinovo-isin-validator
+
+sync style
+
+	var validator = require("cinovo-isin-validator");
+	if (validator("XX000A1G0AE8")) {
+		console.log("ISIN is invalid!");
+	} else {
+		console.log("ISIN is fine!");
+	}
+
+async style
+
+	var validator = require("cinovo-isin-validator");
+	validator("XX000A1G0AE8", function(err) {
+		if (err) {
+			console.log("ISIN is invalid!", err);
+		} else {
+			console.log("ISIN is fine!");
+		}
+	});
+
+## Options
+
+* `checkCountryCode`: Boolean - validate of Country Code (Default: true)
+* `checkCheckDigit`: Boolean -  validate of Check Digit (Default: true)
+
+	var validator = require("cinovo-isin-validator");
+	if (validator("XX000A1G0AE8"), undefined, {checkCountryCode: false, checkCheckDigit: false}) {
+		console.log("ISIN is invalid!");
+	} else {
+		console.log("ISIN is fine!");
+	}
+
+	var validator = require("cinovo-isin-validator");
+	validator("XX000A1G0AE8", function(err) {
+		if (err) {
+			console.log("ISIN is invalid!", err);
+		} else {
+			console.log("ISIN is fine!");
+		}
+	}, {checkCountryCode: false, checkCheckDigit: false});
---
+++ test/isin.js
@@ -0,0 +1,103 @@
+var assert = require("assert-plus"),
+	isin = require("../index");
+
+describe("isin", function () {
+	"use strict";
+	describe("sync", function () {
+		it("invalid character in ISIN", function () {
+			assert.notStrictEqual(isin("EU000a1G0AE8"), undefined);
+		});
+		it("invalid country code", function () {
+			assert.notStrictEqual(isin("XX000A1G0AE8"), undefined);
+		});
+		it("invalid country code without checks", function () {
+			assert.strictEqual(isin("XX000A1G0AE8", undefined, {checkCountryCode: false, checkCheckDigit: false}), undefined);
+		});
+		it("XS country code", function () {
+			assert.strictEqual(isin("XS0832628423"), undefined);
+		});
+		it("EU country code", function () {
+			assert.strictEqual(isin("EU000A1G0AE8"), undefined);
+		});
+		it("invalid check digit", function () {
+			assert.notStrictEqual(isin("DE000A1G0AEA"), undefined);
+		});
+		it("valid EU000A1G0AE8", function () {
+			assert.strictEqual(isin("EU000A1G0AE8"), undefined);
+		});
+		it("valid US298785DF01", function () {
+			assert.strictEqual(isin("US298785DF01"), undefined);
+		});
+		it("valid LU0953782009", function () {
+			assert.strictEqual(isin("LU0953782009"), undefined);
+		});
+		it("valid XS0425743506", function () {
+			assert.strictEqual(isin("XS0425743506"), undefined);
+		});
+		it("valid DE0005557508", function () {
+			assert.strictEqual(isin("DE0005557508"), undefined);
+		});
+	});
+	describe("async", function () {
+		it("invalid character in ISIN", function (done) {
+			isin("EU000a1G0AE8", function(err) {
+				assert.notStrictEqual(err, undefined);
+				done();
+			});
+		});
+		it("invalid country code", function (done) {
+			isin("XX000A1G0AE8", function(err) {
+				assert.notStrictEqual(err, undefined);
+				done();
+			});
+		});
+		it("XS country code", function (done) {
+			isin("XS0832628423", function(err) {
+				assert.strictEqual(err, undefined);
+				done();
+			});
+		});
+		it("EU country code", function (done) {
+			isin("EU000A1G0AE8", function(err) {
+				assert.strictEqual(err, undefined);
+				done();
+			});
+		});
+		it("invalid check digit", function (done) {
+			isin("DE000A1G0AEA", function(err) {
+				assert.notStrictEqual(err, undefined);
+				done();
+			});
+		});
+		it("valid EU000A1G0AE8", function (done) {
+			isin("EU000A1G0AE8", function(err) {
+				assert.strictEqual(err, undefined);
+				done();
+			});
+		});
+		it("valid US298785DF01", function (done) {
+			isin("US298785DF01", function(err) {
+				assert.strictEqual(err, undefined);
+				done();
+			});
+		});
+		it("valid LU0953782009", function (done) {
+			isin("LU0953782009", function(err) {
+				assert.strictEqual(err, undefined);
+				done();
+			});
+		});
+		it("valid XS0425743506", function (done) {
+			isin("XS0425743506", function(err) {
+				assert.strictEqual(err, undefined);
+				done();
+			});
+		});
+		it("valid DE0005557508", function (done) {
+			isin("DE0005557508", function(err) {
+				assert.strictEqual(err, undefined);
+				done();
+			});
+		});
+	});
+});
